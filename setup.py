1b1cb4b1c2a6b1748cc16c0983ce3e7b0efa0e40
joshwills <joshewills@gmail.com>
2019-02-17 21:41:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_usbfileman"
 plugin_name = "OctoPrint-usbFileMan"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.1"
+plugin_version = "0.1.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -27,7 +27,7 @@ plugin_author = "Joshua Wills"
 plugin_author_email = "joshewills@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/joshwills/OctoPrint-usbFileMan"
+plugin_url = "https://github.com/MakerGear/OctoPrint-usbFileMan"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
